fa3d2e6fc36f6e9cbe2dc535bd6e6f047c0b6bb0
Gowtham Alaguraj <gowtham25alaguraj@outlook.com>
2019-01-24 04:10:02 +0000

    Corrected tests

--- tests/homedir_test.go
+++ tests/homedir_test.go
@@ -2,11 +2,16 @@ package tests
 
 import (
    "testing"
+   "os/user"
    "github.com/temp25/hdl/helper"
 )
 
 func TestHomeDir(t *testing.T) {
-   expectedHomeDirectoryPath := "/home/user"
+  user, err := user.Current()
+   if err != nil {
+      panic(err)
+   }
+   expectedHomeDirectoryPath := "/home/" + user.Username
    actualHomeDirectoryPath, _ := helper.HomeDir()
    if expectedHomeDirectoryPath != actualHomeDirectoryPath {
       t.Error("Expected",expectedHomeDirectoryPath , " but got", actualHomeDirectoryPath)
