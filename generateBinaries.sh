84f3a9eaefdc1f78e8cc6a01b4a90c8e5da50938
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-01-24 23:47:02 +0530

    Modified generateBinaries.sh

--- generateBinaries.sh
+++ generateBinaries.sh
@@ -8,7 +8,7 @@ go get github.com/mitchellh/gox
 for osArch in $(gox -osarch-list)
 do
  re="[a-z0-9]+\/[a-z0-9]+"
- if [ $osArch =~ $re ]; then 
+ if [[ $osArch =~ $re ]]; then 
    echo Building binary for $osArch;
    gox -ldflags="-s -w" -osarch="$osArch" -output="artifacts/{{.Dir}}_{{.OS}}_{{.Arch}}"
    echo Binary stored in $(pwd)/artifacts folder
