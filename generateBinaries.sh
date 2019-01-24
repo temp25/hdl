0cb342898aaa9c890566821be99c6691375d3058
Gowtham Alaguraj <gowtham25alaguraj@outlook.com>
2019-01-24 08:01:30 +0000

    Add generate libs script to deploy block

---
+++ generateBinaries.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Compiles binary for available os/arch in go version
+
+#Get Gox for Go cross compilation 
+go get github.com/mitchellh/gox 	
+
+for osArch in $(gox -osarch-list)
+do
+ re="[a-z0-9]+\/[a-z0-9]+"
+ if [[ $osArch =~ $re ]]; then 
+   echo Building binary for $osArch;
+   gox -ldflags="-s -w" -osarch="$osArch" -output="artifacts/{{.Dir}}_{{.OS}}_{{.Arch}}"
+   echo Binary stored in $(pwd)/artifacts folder
+ fi
+done
+
