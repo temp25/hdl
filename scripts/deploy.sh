17860a1a2b78d762b7e4c7403a02d9fb4d48afab
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-01-25 01:36:14 +0530

    Simplified deploy script in travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [[ $TRAVIS_TEST_RESULT == 0 ]]; then 
+#if [[ $TRAVIS_TEST_RESULT == 0 ]]; then 
   go get github.com/mitchellh/gox
   go get github.com/tcnksm/ghr
   gox -ldflags="-s -w" -output="artifacts/{{.Dir}}_{{.OS}}_{{.Arch}}"
   ghr $TRAVIS_TAG artifacts/*
   ls -lah
-else
-  travis_terminate 1
-fi
+#else
+#  travis_terminate 1
+#fi
